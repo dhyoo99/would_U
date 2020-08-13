b22e7a1b03d243fe6f05e2101e27329b7ede4c92
Hwan <jhjung3@snu.ac.kr>
2020-08-14 02:18:04 +0900

    배포 준비(2)

--- project/settings.py
+++ project/settings.py
@@ -128,10 +128,14 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
 
 LOGIN_REDIRECT_URL = "/app/"
--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 import app.views
