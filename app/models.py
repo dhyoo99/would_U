93c3b7e138f31c8c6cbbbcd012eab9ed431c7351
Seongwon Chung <syc1013@korea.ac.kr>
2020-08-13 20:58:10 +0900

    models.py 수정

--- app/models.py
+++ app/models.py
@@ -9,10 +9,15 @@ class Planet(models.Model):
     name = models.CharField(max_length=100)
     #img_src = models.TextField()
 
+    def __str__(self):
+        return self.name
 
 class Qna(models.Model):
     owner = models.OneToOneField(User, on_delete = models.CASCADE, related_name = 'Qna')
 
+    def __str__(self):
+        return self.owner.username
+        
 #질문 목록 data
 class Question(models.Model):
     WEIGHT = ((1,1),(2,2),(3,3))
@@ -20,17 +25,25 @@ class Question(models.Model):
     content = models.TextField()
     weight = models.IntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(3)], choices=WEIGHT) #문제의 점수 가중치 1~3
 
+    def __str__(self):
+        return self.content
+
 #질문에 대한 option data
 class Option(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name= 'options')
     content = models.CharField(max_length=200)
-
+    def __str__(self):
+        return self.content
+     
 
 #QnA와 그 안에 포함된 Question의 관계 나타냄.
 class Qna_question(models.Model):
     Qna = models.ForeignKey(Qna, on_delete = models.CASCADE, related_name= 'Qna_questions')
     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name = 'Qna_questions')
 
+    def __str__(self):
+        return self.Qna.owner.username + '/' + self.question.content
+     
 # QnA owner의 초기 정답
 class Answer(models.Model):
     qna_question = models.ForeignKey(Qna_question, on_delete=models.CASCADE, related_name = 'answers')    
@@ -57,8 +70,15 @@ class Score(models.Model):
     qna = models.ForeignKey(Qna, on_delete = models.CASCADE, related_name = 'scores')
     score = models.IntegerField()
 
+    def __str__(self):
+        return self.user.usrname + '/' + self.qna.owner.username
+     
 #각 행성간 거리
 class Distance(models.Model):
     this = models.ForeignKey(Planet, on_delete=models.CASCADE, related_name= 'distances')
     that = models.ForeignKey(Planet, on_delete=models.CASCADE, related_name= 'that_distances')
     distance = models.IntegerField()
+
+    def __str__(self):
+        return self.this.name + '/' + self.that.name
+     
