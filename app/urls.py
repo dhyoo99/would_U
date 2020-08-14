eeb807b3b51ca5f180093776a57fef0e3fd715dd
dhyoo99 <dhyoo99@naver.com>
2020-08-14 16:21:46 +0900

    almost nothing change

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/signup/', views.signup, name='signup'),
+    path('signup/', views.signup, name='signup'),
     #장고 로그인 기능으로 대체
     # path('/login/', views.login, name='login'),
     # path('/logout/', views.logout, name='logout'),
