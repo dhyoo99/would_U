fb2dfacd2281a8bbd8edfc9a1895d67b7ae5a833
codinghyunman2 <tkso1357@naver.com>
2020-08-15 15:50:00 +0900

    템플릿 이동 추가

--- app/urls.py
+++ app/urls.py
@@ -6,8 +6,14 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
 
+    path('create_qna_home/', views.create_qna_home, name='create_qna_home'),
     path('create_qna/', views.create_qna, name='create_qna'),
+    path('share_qna/', views.share_qna, name='share_qna'),
+
+    path('solve_qna_home/', views.solve_qna_home, name='solve_qna_home'),
     path('solve_qna/<int:qna_pk>', views.solve_qna, name='solve_qna'),
     path('score/<int:score_pk>', views.score, name='score'),
-    path('share_qna/', views.share_qna, name='share_qna'),
+    path('result_qna/', views.result_qna, name='result_qna'), 
+    path('answer_detail/', views.answer_detail, name='answer_detail'), 
+    
 ]
